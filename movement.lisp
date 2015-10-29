c894750d6c162adcc9d5df5c94739275919011be
Elliot Penson <elliotpenson@gmail.com>
2015-10-29 13:16:11 -0500

    Add a function that determines a pieces origin given a destination

--- movement.lisp
+++ movement.lisp
@@ -52,3 +52,21 @@
 (defun can-jump-p (piece-letter)
   "Determines if a piece can move without concern for pieces on its path"
   (char-equal piece-letter #\N))
+
+(defun move-origin (final-file final-rank pre-board piece-letter whitep
+                    capturep &key origin-file origin-rank)
+  "Selects from a piece's possible moves the starting location based on a
+   destination. The pre-board variable gives the layout before the move."
+  (find-if (lambda (possible-move)
+             (destructuring-bind (file-scale rank-scale) possible-move
+               (let ((candidate-file (+ final-file file-scale))
+                     (candidate-rank (+ final-rank rank-scale)))
+                 (and (in-board-p candidate-file candidate-rank)
+                      (eq (get-square pre-board candidate-file candidate-rank)
+                          (piece-char->symbol piece-letter whitep))
+                      (or (can-jump-p piece-letter)
+                          (not (pieces-between-p pre-board final-file final-rank
+                                                 candidate-file candidate-rank)))
+                      (or (null origin-file) (= origin-file candidate-file))
+                      (or (null origin-rank) (= origin-rank candidate-rank))))))
+           (piece-moves piece-letter whitep capturep final-rank)))
