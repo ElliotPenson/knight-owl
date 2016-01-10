c9412977358d2eec04c7c2e8a4486224a0bc3d5f
Elliot Penson <elliotpenson@gmail.com>
2016-01-09 19:15:24 -0600

    Add functions that read chess notation and apply moves to a board

--- movement.lisp
+++ movement.lisp
@@ -3,6 +3,11 @@
 
 (in-package :knight-owl)
 
+(defconstant +algebraic-move-re+
+  "([RNBQK])?([a-h])?([1-8])?(x)?([a-h])([1-8])(=[RNBQK])?([#+])?"
+  "A regular expression that describes chess movement in standard
+   algebraic notation")
+
 (defgeneric piece-moves (piece-char whitep capturep final-rank)
   (:documentation "Evaluates to a list of (file rank) scales"))
 
@@ -49,24 +54,65 @@
               '((0 -1) (0 -2))
               '((0 -1))))))
 
-(defun can-jump-p (piece-letter)
+(defun can-jump-p (piece-char)
   "Determines if a piece can move without concern for pieces on its path"
-  (char-equal piece-letter #\N))
+  (char-equal piece-char #\N))
 
 (defun move-origin (final-file final-rank pre-board piece-letter whitep
                     capturep &key origin-file origin-rank)
   "Selects from a piece's possible moves the starting location based on a
    destination. The pre-board variable gives the layout before the move."
-  (find-if (lambda (possible-move)
-             (destructuring-bind (file-scale rank-scale) possible-move
-               (let ((candidate-file (+ final-file file-scale))
-                     (candidate-rank (+ final-rank rank-scale)))
-                 (and (in-board-p candidate-file candidate-rank)
-                      (eq (get-square pre-board candidate-file candidate-rank)
-                          (piece-char->symbol piece-letter whitep))
-                      (or (can-jump-p piece-letter)
-                          (not (pieces-between-p pre-board final-file final-rank
-                                                 candidate-file candidate-rank)))
-                      (or (null origin-file) (= origin-file candidate-file))
-                      (or (null origin-rank) (= origin-rank candidate-rank))))))
-           (piece-moves piece-letter whitep capturep final-rank)))
+  (loop for (file-scale rank-scale)
+     in (piece-moves piece-letter whitep capturep final-rank)
+     as candidate-file = (+ final-file file-scale)
+     as candidate-rank = (+ final-rank rank-scale)
+     when (and (in-board-p candidate-file candidate-rank)
+               (eq (get-square pre-board candidate-file candidate-rank)
+                   (piece-char->symbol piece-letter whitep))
+               (or (can-jump-p piece-letter)
+                   (not (pieces-between-p pre-board final-file final-rank
+                                          candidate-file candidate-rank)))
+               (or (null origin-file) (= origin-file candidate-file))
+               (or (null origin-rank) (= origin-rank candidate-rank)))
+     return (list candidate-file candidate-rank)))
+
+(defun destructure-move (string-move)
+  "Does the dirty work of parsing a move in algebraic notation. Evaluates to a
+   list of (piece origin-file origin-rank capturep final-file final-rank
+   promotion and check)"
+  (flet ((string->char (string)
+           (char string 0)))
+    (register-groups-bind (piece origin-file origin-rank capturep
+                                 final-file final-rank promotion check)
+        (+algebraic-move-re+ string-move)
+      (list (if piece (string->char piece) #\P)
+            (when origin-file
+              (file->index (string->char origin-file)))
+            (when origin-rank
+              (rank->index (string->char origin-rank)))
+            capturep
+            (file->index (string->char final-file))
+            (rank->index (string->char final-rank))
+            (when promotion
+              ;; choose piece part of '=piece'
+              (char promotion 1))
+            check))))
+
+(defun make-move (move board whitep)
+  "Destructively modifies the board for the given move. The move parameter
+   should be a string in algebraic chess notation."
+  ;; TODO: castling, check/checkmate
+  (destructuring-bind (piece origin-rank origin-file capturep
+                             final-file final-rank promotion check)
+      (destructure-move move)
+    (declare (ignore check))
+    (destructuring-bind (origin-file origin-rank)
+        (move-origin final-file final-rank board piece whitep capturep
+                     :origin-file origin-file :origin-rank origin-rank)
+      (setf (get-square board final-file final-rank)
+            (if promotion
+                (piece-char->symbol promotion whitep)
+                (get-square board origin-file origin-rank))
+                (get-square board origin-file origin-rank)
+                nil)))
+  board)
