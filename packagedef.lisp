4dd20ff9f32b47aa6654548381ce7c077d0ce15d
Elliot Penson <elliotpenson@gmail.com>
2016-03-22 10:55:26 -0500

    Export core library functions

--- packagedef.lisp
+++ packagedef.lisp
@@ -4,4 +4,7 @@
 (ql:quickload "cl-ppcre")
 
 (defpackage :knight-owl
-  (:use :common-lisp :cl-ppcre))
+  (:use :common-lisp :cl-ppcre)
+  (:export :new-board :duplicate-board :print-board :get-square
+           :make-move :valid-move-p :check-p :checkmate-p :+all-moves+
+           :pgn-parse))
